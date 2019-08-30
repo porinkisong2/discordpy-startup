a70adef8944a9fcc32cbb5f48fa1bcecb664f52b
porinkisong <porinkisong@icloud.com>
2019-08-30 21:37:12 +0900

    add help()

--- discordbot.py
+++ discordbot.py
@@ -15,20 +15,36 @@ async def on_command_error(ctx, error):
 async def otintin(ctx):
     await ctx.send('おっぱい！')
 
+
 @bot.command()
 async def oppai(ctx):
     await ctx.send('おちんちん！')
+
 
 @bot.command()
 async def manko(ctx):
     await ctx.send('まんこ！')
 
+
 @bot.command()
 async def uemura(ctx):
     await ctx.send('うえむらのちんちんは小さいよね、、、')
 
+
 @bot.command()
 async def whoTakashi(ctx):
-    await ctx.send('僕は小学三年生のクソガキAIたかし！\n「/otintin」と入力すると「おっぱい！」を返し、「/oppai」と入力すると「おちんちん！」と返す素朴なbotです。\n通常の９歳児と異なりHeroku上で生きているので睡眠を必要としません。')
+    await ctx.send('僕は小学三年生のクソガキAIたかし！\n' +
+                   '「/ otintin」と入力すると「おっぱい！」を返し、' +
+                   '「/oppai」と入力すると「おちんちん！」と返す素朴なbotです。\n' +
+                   '通常の９歳児と異なりHeroku上で生きているので睡眠を必要としません。')
+
+
+@bot.command()
+async def on_message(message):
+    if message.content.startwith("help"):
+        m = ('たかしの持っている関数は以下の通りだよ！\n /otintin(), ' +
+             '/oppai(), /manko(), /uemura() /whoTakashi()' +
+             'うえむらのちんちんはたしかに小さいよね...')
+        await message.channel.send(m)
 
 bot.run(token)
