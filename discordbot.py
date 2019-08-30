9495391e8990e315b6cd5fe6e2b48fe6e88f27bf
sako <s1820454@s.tsukuba.ac.jp>
2019-08-30 20:58:22 +0900

    add kakurituki

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
+import random
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -25,7 +26,15 @@ async def manko(ctx):
 
 @bot.command()
 async def uemura(ctx):
-    await ctx.send('うえむらのちんちんは小さいよね、、、')
+    sentakushi = random.randint(0,2)
+    if sentakushi == 0:
+        await ctx.send('うえむらのちんちんは小さいよね、、、')
+    elif sentakushi == 1:
+        await ctx.send('うえむらのちんちんは普通だよね、、、')
+    elif sentakushi == 2:
+        await ctx.send('うえむらのちんちんはでっかい！！！！')
+
+
 
 @bot.command()
 async def whoTakashi(ctx):
