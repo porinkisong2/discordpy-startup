63c7879fddce1cfb827f9a428985a323f07454cd
sako <s1820454@s.tsukuba.ac.jp>
2019-08-30 23:33:31 +0900

    tyobito kaeta

--- discordbot.py
+++ discordbot.py
@@ -6,7 +6,7 @@ import random
 
 # bot = commands.Bot(command_prefix='/')
 # token = os.environ['DISCORD_BOT_TOKEN']
-TOKEN = 'NjE2OTMxMjQxNTg0NzU0Njg4.XWkylQ.CoZ6ZNZm1Y279-5a6GAl7qqeLuE'
+token = 'NjE2OTMxMjQxNTg0NzU0Njg4.XWkylQ.CoZ6ZNZm1Y279-5a6GAl7qqeLuE'
 client = discord.Client()
 
 
