23857d6b573d4d2a14a99475f86af775ccd3f219
porinkisong2 <44592822+porinkisong2@users.noreply.github.com>
2019-08-30 19:13:12 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,5 +15,12 @@ async def on_command_error(ctx, error):
 async def otintin(ctx):
     await ctx.send('おっぱい！')
 
+@bot.command()
+async def oppai(ctx):
+    await ctx.send('おちんちん！')
+    
+@bot.command()
+async def whoTakashi(ctx):
+    await ctx.send('僕は小学三年生のクソガキAIたかし！\n「/otintin」と入力すると「おっぱい！」を返し、「/oppai」と入力すると「おちんちん！」と返す素朴なbotです。\n通常の９歳児と異なりHeroku上で生きているので睡眠を必要としません。')
 
 bot.run(token)
